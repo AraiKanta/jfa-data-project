aefe76f74c8899674e16820d343983cae0afd008
AraiKanta <arai.kanta@fan-technology.com>
2026-07-06 15:37:05 +0900

    Country、Competition、Match モデルを Django Admin に登録する。

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Competition, Country, Match
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Competition)
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = (
+        'match_date',
+        'competition',
+        'country',
+        'score_japan',
+        'score_opponent',
+        'venue',
+    )
+    list_filter = ('competition', 'match_date')
+    search_fields = ('country__name', 'competition__name', 'venue')
+    date_hierarchy = 'match_date'
